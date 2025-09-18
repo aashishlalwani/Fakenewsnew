caf1bc183827d2176d963c8d16715299884ac5e7
aashishlalwani <126970568+aashishlalwani@users.noreply.github.com>
2025-09-18 15:00:48 +0530

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,8 @@
+def train_model():
+    """Trains the Transformer model."""
+    print("Starting model training...")
+    # This is a placeholder. We will implement this together.
+    pass
+
+if __name__ == '__main__':
+    train_model()
